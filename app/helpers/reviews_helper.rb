65642978ac54b39c47dae501d4facfc61f6e3b32
Paul SCHAUSS <paulschauss@MacBook-Pro-de-Paul.local>
2023-12-07 11:09:26 +0100

    fix: extract original text of google reviews

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,6 @@
 module ReviewsHelper
   def extract_original_text(text)
-    match_data = text.match(/^(.+)\s*\(Translated by Google\)/m)
+    match_data = text.match(/\(Original\)(.+)/m)
     match_data ? match_data[1].strip : text
   end
 
